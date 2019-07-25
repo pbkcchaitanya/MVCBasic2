ac68a22e7d374af0c556531c13d29917a8a9ff88
pbkcchaitanya <pbkcchaitanya@gmail.com>
2019-07-25 14:22:29 +0530

    New Changes to Project reg TEMPLATE BOOTSTRAP-lumen

--- MVCBasic2/App_Start/BundleConfig.cs
+++ MVCBasic2/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MVCBasic2
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-hero.css",
                       "~/Content/site.css"));
         }
     }
--- MVCBasic2/App_Start/RouteConfig.cs
+++ MVCBasic2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MVCBasic2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Movie", action = "Random", id = UrlParameter.Optional }
             );
         }
     }
